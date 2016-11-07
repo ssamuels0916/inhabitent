f51877cab702459c464f8401f587a01f2012689c
Sharifa <shar.samuels@gmail.com>
2016-11-07 13:17:58 -0800

    upated front page and included plugin

---
+++ plugins/red-plugin/lib/functions/taxonomies.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * TAXONOMIES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_taxonomy
+ */
+
+// Register Product Type Taxonomy
+function register_product_type_taxonomy() {
+
+	$labels = array(
+		'name'                       => 'Products Types',
+		'singular_name'              => 'Product Type',
+		'menu_name'                  => 'Product Types',
+		'all_items'                  => 'All Product Types',
+		'parent_item'                => 'Parent Product Type',
+		'parent_item_colon'          => 'Parent Product Type:',
+		'new_item_name'              => 'New Product Type Name',
+		'add_new_item'               => 'Add Product Type',
+		'edit_item'                  => 'Edit Product Type',
+		'update_item'                => 'Update Product Type',
+		'view_item'                  => 'View Product Type',
+		'separate_items_with_commas' => 'Separate product types with commas',
+		'add_or_remove_items'        => 'Add or remove product types',
+		'choose_from_most_used'      => 'Choose from the most used',
+		'popular_items'              => 'Popular Product Types',
+		'search_items'               => 'Search Product Types',
+		'not_found'                  => 'Not Found',
+		'no_terms'                   => 'No Product Types',
+		'items_list'                 => 'product Type list',
+		'items_list_navigation'      => 'product Type list navigation',
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => false,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'product_type', array( 'post' ), $args );
+
+}
+add_action( 'init', 'register_product_type_taxonomy', 0 );
--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -5,36 +5,44 @@
  */
 
 get_header(); ?>
+
     <section class = "hero-image">
         <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="hero-img" alt="inhabitent circle logo" />
     </section>
 
-	<div id = "primary" class="content-area">
-		<main id = "main" class="site-main" role="main">
+<!--	<div id = "primary" class="content-area">
+		<main id = "main" class="site-main" role="main">-->
+
+<!--   Shopping Section -->
+
             <section class = "shop-stuff">
                 <h2>Shop Stuff</h2>
             </section>
+
+<!--   Inhabitent Journal Section -->
+
             <section class = "inhabitent-journal">
                 <h2>Inhabitent Journal</h2>
             </section>   
 
+<!--   Adventure Section -->
             <section class="adventure">
                 <h2>Latest Adventures</h2>
                 <div class = "adventure-block">
 
                     <div class = "left-block">
-                    
+                        <a class  = "adventure-text" href="<?php the_permalink(); ?>">Getting Back to Nature in a Canoe</a>
                     </div>
 
                         <div class = "right-block">
                             <div class = "beach">
-                                
+                              <a class  = "adventure-text" href="<?php the_permalink(); ?>">A Night with Friends at the Beach</a>  
                             </div>
                             <div class = "mountain">
-                                
+                                <a class  = "adventure-text" href="<?php the_permalink(); ?>">Taking in the View at Big Mountain</a> 
                             </div>
                             <div class = "stars">
-
+                                <a class  = "adventure-text" href="<?php the_permalink(); ?>">Star-gazing at the Night Sky</a> 
                             </div>
                         </div>
                     </div>
@@ -42,7 +50,7 @@ get_header(); ?>
             </section>
 			
 
-		</main><!-- #main -->
+		<!--</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
