fd8c5afa91f194d786fa6c964f8dcfaa92f4ea92
Sharifa <shar.samuels@gmail.com>
2016-11-06 21:30:31 -0800

    added adventure section and headers to home page

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -6,20 +6,39 @@
 
 get_header(); ?>
     <section class = "hero-image">
-                     <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="hero-img" alt="inhabitent circle logo" />
+        <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-full.svg'; ?>" class="hero-img" alt="inhabitent circle logo" />
     </section>
 
 	<div id = "primary" class="content-area">
 		<main id = "main" class="site-main" role="main">
             <section class = "shop-stuff">
-
+                <h2>Shop Stuff</h2>
             </section>
-            <section class = "inhabitent-journal>
-            
+            <section class = "inhabitent-journal">
+                <h2>Inhabitent Journal</h2>
             </section>   
 
             <section class="adventure">
-            
+                <h2>Latest Adventures</h2>
+                <div class = "adventure-block">
+
+                    <div class = "left-block">
+                    
+                    </div>
+
+                        <div class = "right-block">
+                            <div class = "beach">
+                                
+                            </div>
+                            <div class = "mountain">
+                                
+                            </div>
+                            <div class = "stars">
+
+                            </div>
+                        </div>
+                    </div>
+
             </section>
 			
 
