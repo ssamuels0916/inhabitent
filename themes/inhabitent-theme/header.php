85bcff0ffa41a4af846b562ea1e75bb06ef987de
Sharifa <shar.samuels@gmail.com>
2016-11-11 23:55:21 -0800

    finally figured out search field and adjusted font-size in widget area

--- themes/inhabitent-theme/header.php
+++ themes/inhabitent-theme/header.php
@@ -25,33 +25,22 @@
       </a>
 
       <header id="masthead" class="site-header" role="banner">
+        <div class = "container">
+          <div class = "logo">
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+            <img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent logo" />
+              </a>
+          </div><!-- end of logo -->
+
         <div class="site-branding">
           <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
         </div>
-
+        
         <nav id="site-navigation" class="main-navigation" role="navigation">
-          <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-<img src="<?php echo get_template_directory_uri() . '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent logo" />
-</a>
-          <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-            <?php esc_html( 'Primary Menu' ); ?>
-          </button>
           <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-            <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-              <label>
-                <input type="search" class="search-field" placeholder="Search" value="" name="s" title="Search efter:">
-              </label>
-              <input type="submit" class="search-submit" value="Search">
-            </form>
-            <!--<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-<label>
-<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-</label>
-<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-</form>-->
-
+          <?php get_search_form(); ?>
         </nav>
+        </div> <!-- end of container -->
         <!-- #site-navigation -->
       </header>
       <!-- #masthead -->
