a09207e1cdd885bd18c29b696edb1e78f7b15e59
Sharifa <shar.samuels@gmail.com>
2016-11-13 22:44:44 -0800

    added js to reload page at top. Just might be last commit

---
+++ themes/inhabitent-theme/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $(function() { //  same as $(document).ready(function() {
+        document.body.scrollTop = 0; // |OR| $('body').scrollTop(0);
+        /* do work */
+    })
+
+});
