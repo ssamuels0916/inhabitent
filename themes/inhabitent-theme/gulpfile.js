0c89eacca26c3ef2dbd8e45a47b972c6db6abd12
Sharifa <shar.samuels@gmail.com>
2016-11-22 15:33:10 -0800

    resolved issue with gulp, added jquery to footer

--- themes/inhabitent-theme/gulpfile.js
+++ themes/inhabitent-theme/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
     ];
 
     browserSync.init(files, {
-        proxy: 'localhost/inhabitent/',
+        proxy: 'http://localhost/inhabitent/', //Change this line to localhost
     });
 
     gulp.watch(files).on('change', browserSync.reload);
--- themes/inhabitent-theme/js/script.js
+++ themes/inhabitent-theme/js/script.js
@@ -1,12 +1,14 @@
 (function($) {
 
     $('.search-submit').on('click', function(event) {
+
         event.preventDefault();
         event.stopPropagation();
         $('.search-field').toggle().blur();
     });
 
     $('.site-content').click(function() {
+        console.log('working?');
         $('.search-field').hide();
     });
 
