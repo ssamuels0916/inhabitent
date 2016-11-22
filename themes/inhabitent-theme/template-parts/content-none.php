647ba4cf0e4df4491dc6dca6875b3aee5c1f833c
Sharifa <shar.samuels@gmail.com>
2016-11-22 10:45:36 -0800

    fixed search field on search no results page

--- themes/inhabitent-theme/template-parts/content-none.php
+++ themes/inhabitent-theme/template-parts/content-none.php
@@ -20,7 +20,14 @@
 		<?php elseif ( is_search() ) : ?>
 
 			<p><?php echo esc_html( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.' ); ?></p>
-			<?php get_search_form(); ?>
+			<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<fieldset>
+		
+		<label>
+			<input type="search" class="search-field-no-results" placeholder="Type and Hit Enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+		</label>
+	</fieldset>
+</form>
 
 		<?php else : ?>
 
