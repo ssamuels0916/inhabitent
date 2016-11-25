4c15430eec49efc444b688a2e4ee692899d806b3
Sharifa <shar.samuels@gmail.com>
2016-11-24 17:55:28 -0800

    added comments

--- themes/inhabitent-theme/front-page.php
+++ themes/inhabitent-theme/front-page.php
@@ -16,32 +16,24 @@ get_header();
 ?>" class="hero-img" alt="inhabitent circle logo" />
   </section>
 
-  <!--	<div id = "primary" class="content-area">
-<main id = "main" class="site-main" role="main">-->
-
   <!--   Shopping Section -->
 
   <section class="shop-stuff">
     <h2>Shop Stuff</h2>
     <div class="shop-section">
-
-      <?php
-$terms = get_terms('product_type');
-foreach ($terms as $term):
-?>
-
-        <?php $url = get_term_link($term->slug, 'product_type');
-?>
-          <div class="shop-stuff-content">
-            <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->slug ?>.svg" />
-
-            <p>
-              <?php echo $term->description ?>
-            </p>
-            <p><a href="<?php echo $url ?>"><?php echo $term->name ?> <span>Stuff</span></a></p>
-          </div>
-          <?php endforeach;
-?>
+      <?php $terms = get_terms('product_type');
+      foreach ($terms as $term):
+      ?>
+      <?php $url = get_term_link($term->slug, 'product_type'); ?>
+    <div class="shop-stuff-content">
+        <!--Shop stuff section icons-->
+        <img src="<?php echo get_template_directory_uri() ?>/images/product-type-icons/<?php echo $term->slug ?>.svg" />
+        <p>
+            <?php echo $term->description ?>
+        </p>
+        <p><a href="<?php echo $url ?>"><?php echo $term->name ?> <span>Stuff</span></a></p>
+    </div>
+        <?php endforeach;?>
     </div>
   </section>
   <!--   Inhabitent Journal Section -->
@@ -49,33 +41,29 @@ foreach ($terms as $term):
     <h2>Inhabitent Journal</h2>
     <div class="get-posts">
       <!--get_posts()-->
+      <!--Returns an array of posts-->
       <?php
-$args = array( 'post_type' => 'post',
-'posts_per_page' => 3,
-'order' => 'DESC',);
-
-$product_posts = get_posts( $args );
-// returns an array of posts
-?>
+          $args = array( 'post_type' => 'post',
+          'posts_per_page' => 3,
+          'order' => 'DESC',);
+          $product_posts = get_posts( $args );
+      ?>
         <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
           <div class="post-content">
             <?php the_post_thumbnail(); ?>
               <div class="journal-info">
                 <p>
                   <?php the_date(); ?> /
-                    <?php $comments_count = wp_count_comments();
-
-echo $comments_count->approved . " Comments"; ?>
+        <?php $comments_count = wp_count_comments();
+              echo $comments_count->approved . " Comments"; ?>
                 </p>
-
                 <h3><a href = "<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 <a class="read" href="<?php the_permalink(); ?>">Read entry</a>
-
               </div>
           </div>
           <?php endforeach;
-wp_reset_postdata();
-?>
+          wp_reset_postdata();
+          ?>
     </div>
   </section>
   <!--   Adventure Section -->
